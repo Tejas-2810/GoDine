5a77f07d9f1ccbf88dbc0cec058f0d73d348a265
Krishna Vaibhav Yadlapalli <kr732880@dal.ca>
2024-02-14 02:32:11 -0400

    feat: added basic routes to the web applicaiton.

---
+++ godine/src/components/navbar/navbar.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import logo from '../../images/logo.png';
+import './navbar.css';
+import { useNavigate } from 'react-router-dom';
+import { Navbar, Nav } from 'react-bootstrap';
+
+const Navb = () => {
+    const navigate = useNavigate();
+    const redirect = () => {
+        navigate('/profile');
+    }
+
+    return (
+        <Navbar bg="transparent" variant="dark" expand="lg">
+            <Navbar.Brand href="/">
+                <img className="mx-3" src={logo} alt="Logo" height={50}/>
+               GoDine
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse className='justify-content-end' id="basic-navbar-nav ">
+                <Nav className="m-4 h6 text-center ">
+                    <Nav.Link href="/" className="text-dark tw">Home</Nav.Link>
+                    <Nav.Link className="text-dark tw">About</Nav.Link>
+                    <Nav.Link onClick={redirect} className="text-dark tw">Profile</Nav.Link>
+                    <Nav.Link className="text-dark tw" >Contact</Nav.Link>
+                </Nav>
+
+
+
+            </Navbar.Collapse>
+        </Navbar>
+        
+    );
+}
+
+export default Navb;
