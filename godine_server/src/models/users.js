cd61c568b5cbacea1848444442f00dc50d8485be
Jahnavi Gajjala <jh707761@dal.ca>
2024-02-18 16:40:06 -0400

    feat: Server-side code for authentication

--- godine_server/src/models/users.js
+++ godine_server/src/models/users.js
@@ -1,5 +1,8 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const crypto = require('crypto');
 
+// Create a new user schema
 const userSchema = new mongoose.Schema({
   username: {
     type: String,
@@ -14,9 +17,36 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
     unique: true
-  }
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  isRestaurantOwner: {
+    type: Boolean,
+    default: false
+  },
+  passwordResetToken: String,
+  passwordResetExpires: Date
 },{ collection: 'Users' });
 
+// Hash the password before saving it to the database
+userSchema.pre('save', async function(next) {
+  if (!this.isModified('password')) return next();
+  this.password = await bcrypt.hash(this.password, 12);
+  next();
+});
+
+// Reset password token generation for 30 minutes
+userSchema.methods.createPasswordResetToken = function() {
+  const resetToken = crypto.randomBytes(32).toString('hex');
+  this.passwordResetToken = crypto.createHash('sha256').update(resetToken).digest('hex');
+  this.passwordResetExpires = Date.now() + 30 * 60 * 1000;
+  return resetToken;
+};
+
+// Create a new user model
 const users = mongoose.model('Users', userSchema);
 
+// Export the user model
 module.exports = users;
