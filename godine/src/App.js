273b86231bb922578843b88768b73d0ae1ad5a00
Krishna Vaibhav Yadlapalli <kr732880@dal.ca>
2024-02-19 17:53:10 -0400

    feat: added table reservatoin page.

--- godine/src/App.js
+++ godine/src/App.js
@@ -4,6 +4,7 @@ import Profile from './views/profile/profile';
 import Home from './views/home/home';
 import Navbar from './components/navbar/navbar';
 import WishList from './views/wishlist/wishlist';
+import Reserve from './views/reservation/reserve';
 import P404 from './views/p404/p404';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
@@ -16,6 +17,7 @@ function App() {
           <Route path="/" element={<Home/>} />
           <Route path="/profile" element={<Profile />} />
           <Route path="/wishlist" element={<WishList />} />
+          <Route path="/reserve" element={<Reserve />} />
           <Route path="*" element={<P404 />} />
         </Routes>
       </Router>
