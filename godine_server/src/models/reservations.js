2e000b354deab7eeb5c8dfde04b82ef75e573d50
Praveen Kumar Reddy Burla <pr441109@dal.ca>
2024-02-19 15:40:57 -0400

    feat:added models

---
+++ godine_server/src/models/reservations.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+
+// Create a new reservation schema
+const reservationSchema = new mongoose.Schema(
+  {
+    Reservation_ID: mongoose.Schema.Types.ObjectId,
+    User_ID: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Users", // Reference to the Users collection
+      required: true,
+    },
+    restaurantName: {
+      type: String,
+      required: true,
+    },
+    reservationTime: {
+      type: Date,
+      required: true,
+    },
+    noOfGuests: {
+      type: Number,
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: ["Active", "Cancel"],
+      default: "Active",
+    },
+  },
+  { collection: "Reservations" }
+);
+
+// Create a new reservation model
+const Reservation = mongoose.model("Reservations", reservationSchema);
+
+// Export the reservation model
+module.exports = Reservation;
---
+++ godine_server/src/models/restaurant.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+
+// Create a new restaurant schema
+const restaurantSchema = new mongoose.Schema(
+  {
+    RestaurantID: mongoose.Schema.Types.ObjectId,
+    restaurantName: {
+      type: String,
+      required: true,
+    },
+    restaurantAddress: {
+      type: String,
+      required: true,
+    },
+    availability: {
+      type: [String],
+      enum: ["morning", "evening", "night"],
+      required: true,
+    },
+    operatingHours: {
+      type: String,
+      required: true,
+    },
+    averagePrice: {
+      type: Number,
+      required: true,
+    },
+  },
+  { collection: "Restaurants" }
+);
+
+// Create a new restaurant model
+const Restaurant = mongoose.model("Restaurants", restaurantSchema);
+
+// Export the restaurant model
+module.exports = Restaurant;
