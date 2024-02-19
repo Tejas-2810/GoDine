b6753d4b18aa951c2d3790bb1c2bf77f5eaed89e
Priyatam Reddy Somagattu <pr889078@dal.ca>
2024-02-19 16:32:12 -0400

    feat: bg image for pages and routes in App

--- godine/src/App.js
+++ godine/src/App.js
@@ -3,6 +3,10 @@ import React from 'react';
 import Profile from './views/profile/profile';
 import Home from './views/home/home';
 import Navbar from './components/navbar/navbar';
+import Signin from './views/authentication/signin';
+import Signup from './views/authentication/signup';
+import ForgotPassword from './views/authentication/forgotPassword';
+import ResetPassword from './views/authentication/resetPassword';
 import WishList from './views/wishlist/wishlist';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
@@ -13,6 +17,10 @@ function App() {
         <Navbar />
         <Routes >
           <Route path="/" element={<Home/>} />
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+          <Route path="/reset-password/:token" element={<ResetPassword />} />
           <Route path="/profile" element={<Profile />} />
           <Route path="/wishlist" element={<WishList />} />
         </Routes>
