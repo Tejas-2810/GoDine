69a3b61448991ee3b4131910cdaf457350405006
Praveen Kumar Reddy Burla <pr441109@dal.ca>
2024-03-14 01:00:35 -0300

    wishlist for backend completed

--- godine_server/src/controllers/userController.js
+++ godine_server/src/controllers/userController.js
@@ -42,28 +42,112 @@ exports.updateUserProfile = async (req, res) => {
 
 // Controller to update user's newsletter subscription status
 exports.updateNewsletterSubscription = async (req, res) => {
-    try {
-        const userID = req.params.userID;
-        const user = await Users.findOne({ _id: userID });
-        if (!user) {
-            return res.status(404).json({ message: "User not found" });
-        }
-
-        const subscriptionStatus = req.body.newsLetterStatus;
-        let newsletter = await Newsletter.findOne({ userID: userID});
-
-        if (!newsletter) {
-            newsletter = new Newsletter({
-                userID: userID,
-                newsLetterStatus: subscriptionStatus,
-            });
-        } else {
-            newsletter.newsLetterStatus = subscriptionStatus;
-        }
-
-        await newsletter.save();
-        res.json({ message: "Newsletter subscription status updated" });
-    } catch (err) {
-        res.status(500).json({ message: err.message });
+  try {
+    const userID = req.params.userID;
+    const user = await Users.findOne({ _id: userID });
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
     }
+
+    const subscriptionStatus = req.body.newsLetterStatus;
+    let newsletter = await Newsletter.findOne({ userID: userID });
+
+    if (!newsletter) {
+      newsletter = new Newsletter({
+        userID: userID,
+        newsLetterStatus: subscriptionStatus,
+      });
+    } else {
+      newsletter.newsLetterStatus = subscriptionStatus;
+    }
+
+    await newsletter.save();
+    res.json({ message: "Newsletter subscription status updated" });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+exports.addRestaurantToWishlist = async (req, res) => {
+  try {
+    const userID = req.params.userID;
+    const restaurantID = req.body.restaurantID;
+    const user = await Users.findById(userID);
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    // Add restaurant to wishlist if not already present
+    if (!user.wishlist.includes(restaurantID)) {
+      user.wishlist.push(restaurantID);
+      await user.save();
+      return res.json({ message: "Restaurant added to wishlist" });
+    } else {
+      return res
+        .status(400)
+        .json({ message: "Restaurant already in wishlist" });
+    }
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+exports.removeRestaurantFromWishlist = async (req, res) => {
+  try {
+    const userID = req.params.userID;
+    const restaurantID = req.body.restaurantID;
+
+    // Check if restaurantID is provided in the request body
+    if (!restaurantID) {
+      return res.status(400).json({ message: "Restaurant ID is required" });
+    }
+
+    const user = await Users.findById(userID);
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    // Capture the initial length of the wishlist
+    const initialLength = user.wishlist.length;
+
+    // Safely filter the wishlist, avoiding errors on null values
+    user.wishlist = user.wishlist.filter(
+      (item) => item && item.toString() !== restaurantID.toString()
+    );
+
+    // Check if the wishlist length has changed after the filter operation
+    if (initialLength === user.wishlist.length) {
+      // If lengths are equal, the restaurant ID was not found in the wishlist
+      return res
+        .status(404)
+        .json({ message: "Restaurant does not exist in wishlist" });
+    }
+
+    await user.save();
+
+    res.json({ message: "Restaurant removed from wishlist" });
+  } catch (err) {
+    console.error(err); // Log the error for detailed debugging
+    res.status(500).json({
+      message:
+        "An error occurred while removing the restaurant from the wishlist.",
+    });
+  }
+};
+
+exports.getUserWishlist = async (req, res) => {
+  try {
+    console.log("Inside");
+    const userID = req.params.userID;
+    const user = await Users.findById(userID).populate("wishlist");
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    res.json(user.wishlist);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
 };
--- godine_server/src/routes/userRoutes.js
+++ godine_server/src/routes/userRoutes.js
@@ -1,15 +1,26 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const userController = require('../controllers/userController');
+const userController = require("../controllers/userController");
 
 // GET user profile
-router.get('/:userID', userController.getUserProfile);
+router.get("/:userID", userController.getUserProfile);
 
 // PUT update user profile
-router.put('/edit/:userID', userController.updateUserProfile);
+router.put("/edit/:userID", userController.updateUserProfile);
 
 // PUT update user's newsletter subscription status
-router.put('/subscribe/:userID', userController.updateNewsletterSubscription);
+router.put("/subscribe/:userID", userController.updateNewsletterSubscription);
+
+// POST add a restaurant to wishlist
+router.post("/wishlist/add/:userID", userController.addRestaurantToWishlist);
+
+// DELETE remove a restaurant from wishlist
+router.delete(
+  "/wishlist/remove/:userID",
+  userController.removeRestaurantFromWishlist
+);
+
+// GET get user's wishlist
+router.get("/wishlist/:userID", userController.getUserWishlist);
 
 module.exports = router;
-
