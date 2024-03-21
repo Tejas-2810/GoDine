3fa630d93e529b173016242328e7f11980fdef49
Tejas <tj970639@dal.ca>
2024-03-20 21:50:57 -0300

    feat: Restaurant Dashboard &  Restaurant Table Booking Page & Home Page required Endpoints with images management

--- godine_server/src/routes/restaurantRoutes.js
+++ godine_server/src/routes/restaurantRoutes.js
@@ -1,16 +1,47 @@
 const express = require('express');
+const multer = require('multer');
 const router = express.Router();
-const restaurantController = require('../controllers/restaurantController'); // Adjust the path as necessary
+const restaurantController = require('../controllers/restaurantController');
+const { checkAuth } = require('../middlewares/authMiddleware');
+
+// Restaurant Dashboard
+router.get('/myrestaurants', checkAuth, restaurantController.getMyRestaurants);
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'uploads/');
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + '-' + file.originalname);
+  }
+});
+
+const upload = multer({ storage: storage });
 
 
-router.post('/', restaurantController.createRestaurant);
+router.post('/createRestaurants', checkAuth, upload.fields([
+  { name: 'menu', maxCount: 1 },
+  { name: 'photos', maxCount: 5 }
+]), restaurantController.createRestaurant);
 
-router.get('/', restaurantController.getAllRestaurants);
+router.put(
+    '/updateRestaurant/:restaurantID', 
+    checkAuth, 
+    upload.fields([
+      { name: 'menu', maxCount: 1 },
+      { name: 'photos', maxCount: 5 }
+    ]),
+    restaurantController.updateRestaurantById
+  );
 
-router.get('/:restaurantID', restaurantController.getRestaurantById);
 
 router.put('/:restaurantID', restaurantController.updateRestaurantById);
 
-router.delete('/:restaurantID', restaurantController.deleteRestaurantById);
 
+// Restaurant Page
+router.get('/:restaurantID', restaurantController.getRestaurantById);
+
+//Home page
+router.get('/', restaurantController.getAllRestaurantsForHomePage);
 module.exports = router;
+
