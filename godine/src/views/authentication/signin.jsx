25b9df0ded897b1fd1b35befe5ac830617db9f1c
Priyatam Reddy Somagattu <tech.nerd141@gmail.com>
2024-03-29 03:44:57 -0300

    fix: changed and cleaned code in signup and signin

--- godine/src/views/authentication/signin.jsx
+++ godine/src/views/authentication/signin.jsx
@@ -1,4 +1,4 @@
-import React, {useState, useRef} from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 import { Link, useNavigate, useLocation } from 'react-router-dom'
 import useAuth from '../../hooks/useAuth';
 import './common.css'
@@ -7,25 +7,28 @@ import axios from 'axios';
 function Signin() {
     const [email, setEmail] = useState('');
     const [validEmail, setValidEmail] = useState(true);
+    const emailRef = useRef();
 
     const [password, setPassword] = useState('');
     const [validPassword, setValidPassword] = useState(true);
     const requestCancelRef = useRef(null);
 
-    const { setAuthData } = useAuth();
+    const { setAuthData, clearAuthData } = useAuth();
+
+    useEffect( () => {
+        clearAuthData();
+        emailRef.current.focus();
+    }, [clearAuthData]);
 
     const navigate = useNavigate();
     const location = useLocation();
 
     function validateEmailAndSet(e) {
         const email = e.target.value;
-        // email is an email
         const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
-
         if (emailRegex.test(email)) {
             setValidEmail(true);
             setEmail(email);
-            
         }
         else {
             setValidEmail(false);
@@ -35,7 +38,6 @@ function Signin() {
     function validatePasswordAndSet(e) {
         const pwd = e.target.value;
         const pwdRegex = /^.{8,}$/;
-
         if (pwdRegex.test(pwd)) {
             setValidPassword(true);
             setPassword(pwd);
@@ -45,77 +47,75 @@ function Signin() {
         }
     }
 
-    async function handleSubmit(e){
+    async function handleSubmit(e) {
         e.preventDefault();
 
         const userEmail = email;
         const pwd = password;
         const from = location.state;
 
-        const server_url = process.env.SERVER_URL || "http://localhost";
-        const server_port = process.env.SERVER_PORT || "8080";
+        const server_url = process.env.REACT_APP_SERVER_URL || "http://localhost";
+        const server_port = process.env.REACT_APP_SERVER_PORT || "8080";
+        const signin_endpoint = process.env.REACT_APP_SIGNIN_ENDPOINT || "api/auth/signin";
 
         requestCancelRef.current?.abort();
         requestCancelRef.current = new AbortController();
 
         // send the email and password to the server
-        if(userEmail !== '' && pwd !== '' && validEmail && validPassword){
-            const url = `${server_url}:${server_port}/api/auth/signin`;
-            const data = {email: userEmail, password: pwd};
+        if (userEmail !== '' && pwd !== '' && validEmail && validPassword) {
+            const url = `${server_url}:${server_port}/${signin_endpoint}`;
+            const data = { 
+                email: userEmail, 
+                password: pwd 
+            };
 
-            const response = await axios.post(url,data, {signal: requestCancelRef.current?.signal})
-            .then((response) => response)
-            .catch((err) => {
-                if(axios.isCancel(err)){
+            const response = await axios.post(url, data, { signal: requestCancelRef.current?.signal })
+                .then((response) => response)
+                .catch((err) => {
+                    if (axios.isCancel(err)) {
+                        return err;
+                    }
+                    if (axios.isAxiosError(err)) {
+                        return err.response;
+                    }
                     return err;
-                }
-                if(axios.isAxiosError(err)){
-                    return err.response;
-                }
-                return err;
-            });
+                });
 
             // when request is aborted
-            if(axios.isCancel(response)){
+            if (axios.isCancel(response)) {
                 console.log("Signin Request Aborted");
                 return;
             }
 
-            if(response.status === 200 && response.data?.token && response.data?.role){
+            if (response.status === 200 && response.data?.token && response.data?.role) {
                 const token = response.data.token;
-                // todo: check the updated response
-                const responseExpiryTime = response.data.JWTExpiryMin;
-                // default expiry of 20 mins in milliseconds
-                const defaultExpiryTime = 20*60*1000;
-                const expiryTime = responseExpiryTime ? responseExpiryTime: defaultExpiryTime;
                 const role = response.data.role;
 
-                setAuthData(token, expiryTime, role);
+                setAuthData(token, role);
 
                 // navigate to respective webpages as per role
-                if(role === 'user'){
-                    navigate(from?.pathname ? from.pathname: '/', {replace: true});
+                if (role === 'user') {
+                    navigate(from?.pathname ? from.pathname : '/', { replace: true });
                 }
-                else if(role === 'restaurant owner'){
-                    // todo: redirect to restaurant owner dashboard
-                    navigate('/', {replace: true});
+                else if (role === 'restaurant owner') {
+                    navigate(from?.pathname ? from.pathname: '/dashboard', { replace: true });
                 }
-                else{
-                    navigate('/', {replace: true});
+                else {
+                    navigate('/', { replace: true });
                 }
                 return;
             }
-            else if(response.status === 401){
+            else if (response.status === 401) {
                 alert(response.data.message);
             }
-            else if(response.status === 500){
+            else if (response.status === 500) {
                 alert('Internal server error!');
             }
-            else{
+            else {
                 alert('Please again later after sometime!');
             }
         }
-        else{
+        else {
             alert('Invalid email or password');
         }
     }
@@ -126,21 +126,21 @@ function Signin() {
                 <div className='row justify-content-center'>
                     <div className='col-sm-6'>
                         <div className='card signin-card glass'>
-                            <h1 style={{color: "#333333"}}>Sign In</h1>
+                            <h1 style={{ color: "#333333" }}>Sign In</h1>
                             <form>
                                 <div className='form-group m-1'>
                                     <label>Email</label>
-                                    <input type='text' className='form-control' placeholder='Enter email' onInput={validateEmailAndSet}/>
-                                    {validEmail ? null : <small style={{color: 'red'}}>Please enter a valid email</small>}
+                                    <input type='text' ref={emailRef} className='form-control' placeholder='Enter email' onInput={validateEmailAndSet} />
+                                    {validEmail ? null : <small style={{ color: 'red' }}>Please enter a valid email</small>}
                                 </div>
                                 <div className='form-group m-1'>
                                     <label>Password</label>
-                                    <input type='password' className='form-control' placeholder='Enter password' onInput={validatePasswordAndSet}/>
-                                    {validPassword ? null : <small style={{color: 'red'}}>Min. 8 characters</small>}
+                                    <input type='password' className='form-control' placeholder='Enter password' onInput={validatePasswordAndSet} />
+                                    {validPassword ? null : <small style={{ color: 'red' }}>Min. 8 characters</small>}
                                 </div>
 
-                                <Link to="/forgot-password" style={{display: "block"}} >Forgot Password?</Link>
-                                <Link to="/signup" style={{display: "block"}}>Don't have an account? Create one now</Link>
+                                <Link to="/forgot-password" style={{ display: "block" }} >Forgot Password?</Link>
+                                <Link to="/signup" style={{ display: "block" }}>Don't have an account? Create one now</Link>
 
                                 <button type='submit' className='btn btn-primary m-1' onClick={handleSubmit} >Submit</button>
                             </form>
@@ -149,7 +149,7 @@ function Signin() {
                 </div>
             </div>
         </div>
-    )
+    );
 }
 
 export default Signin;
