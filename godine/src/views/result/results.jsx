7b03f92636580c3b54c78f4c8d1b61d2ecf666f3
Praveen Kumar Reddy Burla <pr441109@dal.ca>
2024-04-01 00:21:29 -0300

    done the modifications for the wishlist

--- godine/src/views/result/results.jsx
+++ godine/src/views/result/results.jsx
@@ -1,180 +1,253 @@
-import React from 'react';
-import './results.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min';
-import { useSearchParams } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import "./results.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { useSearchParams } from "react-router-dom";
+import { AiFillHeart, AiOutlineHeart } from "react-icons/ai";
+import useAuth from "../../hooks/useAuth";
 
 const Results = () => {
-    const [searchParams] = useSearchParams();
-    console.log("testing",searchParams.get('c')); // Output: Any Cuisine
-    return (
-        <div className='container cb'>
-            <section>
-                <div className="row">
-                    <div className="col-md-3">
-                        <div className="dropdown">
-                            <button
-                                className="btn btn-primary dropdown-toggle"
-                                type="button"
-                                id="discountDropdown"
-                                data-toggle="dropdown"
-                                aria-haspopup="true"
-                                aria-expanded="false"
-                            >
-                            Discounts
-                            </button>
-                            <div className="dropdown-menu" aria-labelledby="discountDropdown">
-                                <div>
-                                    <input
-                                        type="checkbox"
-                                        id="discount-10"
-                                        value="10"
-                                    />
-                                    <label htmlFor="discount-10">10%</label>
-                                </div>
-                                <div>
-                                    <input
-                                        type="checkbox"
-                                        id="discount-20"
-                                        value="20"
-                                    />
-                                    <label htmlFor="discount-20">20%</label>
-                                </div>
-                                <div>
-                                    <input
-                                        type="checkbox"
-                                        id="discount-30"
-                                        value="30"
-                                    />
-                                    <label htmlFor="discount-30">30%</label>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-md-3">
-                        <div className="dropdown">
-                            <button
-                                className="btn btn-primary dropdown-toggle"
-                                type="button"
-                                id="locationDropdown"
-                                data-toggle="dropdown"
-                                aria-haspopup="true"
-                                aria-expanded="false"
-                            >
-                                Location
-                            </button>
-                            <div className="dropdown-menu" aria-labelledby="locationDropdown">
-                                <a className="dropdown-item">New York</a>
-                                <a className="dropdown-item">London</a>
-                                <a className="dropdown-item">Halifax</a>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-md-3">
-                        <div className="dropdown">
-                            <button
-                                className="btn btn-primary dropdown-toggle"
-                                type="button"
-                                id="cuisineDropdown"
-                                data-toggle="dropdown"
-                                aria-haspopup="true"
-                                aria-expanded="false"
-                            >
-                                Cuisine
-                            </button>
-                            <div className="dropdown-menu" aria-labelledby="cuisineDropdown">
-                                <a className="dropdown-item">Italian</a>
-                                <a className="dropdown-item">Mexican</a>
-                                <a className="dropdown-item">Chinese</a>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-md-3">
-                        <div className="dropdown">
-                            <button
-                                className="btn btn-primary dropdown-toggle"
-                                type="button"
-                                id="ratingsDropdown"
-                                data-toggle="dropdown"
-                                aria-haspopup="true"
-                                aria-expanded="false"
-                            >
-                                Ratings
-                            </button>
-                            <div className="dropdown-menu" aria-labelledby="ratingsDropdown">
-                                <a className="dropdown-item">1</a>
-                                <a className="dropdown-item">2</a>
-                                <a className="dropdown-item">3</a>
-                                <a className="dropdown-item">4</a>
-                                <a className="dropdown-item">5</a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </section>
+  const [searchParams] = useSearchParams();
+  const [restaurants, setRestaurants] = useState([]);
+  const [discounts, setDiscounts] = useState([]);
+  // Adjusted for direct integer parsing of ratings
+  const [ratings, setRatings] = useState({});
+  const [filtersOpen, setFiltersOpen] = useState(false);
+  const [selectedFilters, setSelectedFilters] = useState({
+    location: "",
+    cuisine: "",
+    rating: 0,
+    discount: 0,
+  });
+  const [favorites, setFavorites] = useState({});
+  const { getUserId } = useAuth();
 
-            <section>
-                <div className=" container row">
-                    <div className="col-sm-4">
-                        <div className="card mb-3">
-                            <img src="https://via.placeholder.com/150" className="card-img-top" alt="Restaurant" />
-                            <div className="card-body">
-                                <h5 className="card-title">Restaurant Name</h5>
-                                <p className="m-0">Restaurant Description</p>
-                                <p className="m-0">Discount: 10%</p>
-                                <p className="m-0">Location: New York</p>
-                                <p className="m-0">Cuisine: Italian</p>
-                                <p className="m-0">Ratings: 4.5</p>
-                                <button className="btn btn-primary">Book</button>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-md-4" >
-                        <div className="card mb-3">
-                            <img src="https://via.placeholder.com/150" className="card-img-top" alt="Restaurant" />
-                            <div className="card-body">
-                                <h5 className="card-title">Restaurant Name</h5>
-                                <p className="m-0">Restaurant Description</p>
-                                <p className="m-0">Discount: 10%</p>
-                                <p className="m-0">Location: New York</p>
-                                <p className="m-0">Cuisine: Italian</p>
-                                <p className="m-0">Ratings: 4.5</p>
-                                <button className="btn btn-primary">Book</button>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-md-4 cds">
-                        <div className="card mb-3">
-                            <img src="https://via.placeholder.com/150" className="card-img-top" alt="Restaurant" />
-                            <div className="card-body">
-                                <h5 className="card-title">Restaurant Name</h5>
-                                <p className="m-0">Restaurant Description</p>
-                                <p className="m-0">Discount: 10%</p>
-                                <p className="m-0">Location: New York</p>
-                                <p className="m-0">Cuisine: Italian</p>
-                                <p className="m-0">Ratings: 4.5</p>
-                                <button className="btn btn-primary">Book</button>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-md-4 cds">
-                        <div className="card mb-3">
-                            <img src="https://via.placeholder.com/150" className="card-img-top" alt="Restaurant" />
-                            <div className="card-body">
-                                <h5 className="card-title">Restaurant Name</h5>
-                                <p className="m-0">Restaurant Description</p>
-                                <p className="m-0">Discount: 10%</p>
-                                <p className="m-0">Location: New York</p>
-                                <p className="m-0">Cuisine: Italian</p>
-                                <p className="m-0">Ratings: 4.5</p>
-                                <button className="btn btn-primary">Book</button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </section>
-        </div>
+  useEffect(() => {
+    const fetchRestaurants = async () => {
+      const response = await fetch("http://localhost:8080/api/restaurants/");
+      const data = await response.json();
+      setRestaurants(data);
+      fetchRatings(data);
+    };
+
+    const fetchDiscounts = async () => {
+      const response = await fetch(
+        "http://127.0.0.1:8080/api/discountsPromotions/discounts"
+      );
+      const data = await response.json();
+      setDiscounts(data);
+    };
+
+    const fetchRatings = (restaurants) => {
+      restaurants.forEach(async (restaurant) => {
+        const response = await fetch(
+          `http://127.0.0.1:8080/api/restaurants/${restaurant._id}/reviews`
+        );
+        response.json().then((data) => {
+          setRatings((prevRatings) => ({
+            ...prevRatings,
+            [restaurant._id]: parseInt(data.averageRating),
+          }));
+        });
+      });
+    };
+
+    fetchRestaurants().catch(console.error);
+    fetchDiscounts().catch(console.error);
+  }, []);
+
+  //   const findDiscountForRestaurant = (restaurantId) => {
+  //     const discount = discounts.find(
+  //       (discount) => discount.restaurantID === restaurantId
+  //     );
+  //     return discount ? discount.discountPercentage : 0;
+  //   };
+  const findDiscountForRestaurant = (restaurantId) => {
+    if (!Array.isArray(discounts)) {
+      console.warn("Discounts is not an array", discounts);
+      return 0; // Default to 0 if discounts is not an array
+    }
+    const discount = discounts.find(
+      (discount) => discount.restaurantID === restaurantId
     );
+    return discount ? discount.discountPercentage : 0;
+  };
+
+  const toggleFilters = () => {
+    setFiltersOpen(!filtersOpen);
+  };
+
+  const handleFilterChange = (filter, value) => {
+    setSelectedFilters({ ...selectedFilters, [filter]: parseInt(value) });
+  };
+
+  const toggleFavorite = (restaurantId) => {
+    setFavorites((prevFavorites) => ({
+      ...prevFavorites,
+      [restaurantId]: !prevFavorites[restaurantId],
+    }));
+  };
+
+  const getUniqueCuisines = (restaurants) => {
+    const cuisines = restaurants.map((restaurant) => restaurant.cuisine);
+    return [...new Set(cuisines)];
+  };
+
+  const getUniqueLocations = (restaurants) => {
+    const locations = restaurants.map((restaurant) =>
+      restaurant.restaurantAddress.split(",")[1].trim()
+    );
+    return [...new Set(locations)];
+  };
+
+  const applyFilters = (restaurant) => {
+    const matchesLocation = selectedFilters.location
+      ? restaurant.restaurantAddress.includes(selectedFilters.location)
+      : true;
+    const matchesCuisine = selectedFilters.cuisine
+      ? restaurant.cuisine === selectedFilters.cuisine
+      : true;
+    const rating = ratings[restaurant._id];
+    const matchesRating =
+      rating !== undefined ? parseInt(rating) >= selectedFilters.rating : false; // Ensures filter works correctly when specific ratings are not found
+    const matchesDiscount = selectedFilters.discount
+      ? findDiscountForRestaurant(restaurant._id) === selectedFilters.discount
+      : true;
+
+    return (
+      matchesLocation && matchesCuisine && matchesRating && matchesDiscount
+    );
+  };
+
+  const filteredRestaurants = restaurants.filter(applyFilters);
+
+  const uniqueCuisines = getUniqueCuisines(restaurants);
+  const uniqueLocations = getUniqueLocations(restaurants);
+
+  return (
+    <div className="container cb">
+      <button className="btn btn-primary" onClick={toggleFilters}>
+        {filtersOpen ? "Hide Filters" : "Show Filters"}
+      </button>
+
+      {filtersOpen && (
+        <div className="filter-panel">
+          {/* Filter UI code */}
+          <div className="filter-group">
+            <label htmlFor="location-filter">Location</label>
+            <select
+              id="location-filter"
+              className="form-control"
+              onChange={(e) => handleFilterChange("location", e.target.value)}
+            >
+              <option value="">All Locations</option>
+              {uniqueLocations.map((location, idx) => (
+                <option key={idx} value={location}>
+                  {location}
+                </option>
+              ))}
+            </select>
+          </div>
+          <div className="filter-group">
+            <label htmlFor="cuisine-filter">Cuisine</label>
+            <select
+              id="cuisine-filter"
+              className="form-control"
+              onChange={(e) => handleFilterChange("cuisine", e.target.value)}
+            >
+              <option value="">All Cuisines</option>
+              {uniqueCuisines.map((cuisine, idx) => (
+                <option key={idx} value={cuisine}>
+                  {cuisine}
+                </option>
+              ))}
+            </select>
+          </div>
+          <div className="filter-group">
+            <label htmlFor="rating-filter">Rating</label>
+            <select
+              id="rating-filter"
+              className="form-control"
+              onChange={(e) => handleFilterChange("rating", e.target.value)}
+            >
+              <option value="0">All Ratings</option>
+              <option value="1">1 Star</option>
+              <option value="2">2 Stars</option>
+              <option value="3">3 Stars</option>
+              <option value="4">4 Stars</option>
+              <option value="5">5 Stars</option>
+            </select>
+          </div>
+          <div className="filter-group">
+            <label htmlFor="discount-filter">Discount</label>
+            <select
+              id="discount-filter"
+              className="form-control"
+              onChange={(e) => handleFilterChange("discount", e.target.value)}
+            >
+              <option value="0">Any Discount</option>
+              <option value="10">10% Off</option>
+              <option value="20">20% Off</option>
+              <option value="30">30% Off</option>
+            </select>
+          </div>
+        </div>
+      )}
+
+      <section className="restaurant-list">
+        <div className="row">
+          {filteredRestaurants.map((restaurant, index) => (
+            <div
+              key={index}
+              className="col-sm-4"
+              onClick={() => console.log("Restaurant ID:", restaurant._id)}
+            >
+              <div className="card mb-3">
+                <img
+                  src={
+                    restaurant.photos[0] || "https://via.placeholder.com/150"
+                  }
+                  className="card-img-top"
+                  alt={restaurant.restaurantName}
+                />
+                <div className="card-body">
+                  <div
+                    className="favorite-icon"
+                    onClick={(e) => {
+                      e.stopPropagation(); // Prevent card click event
+                      toggleFavorite(restaurant._id);
+                    }}
+                  >
+                    {favorites[restaurant._id] ? (
+                      <AiFillHeart size={24} color="red" />
+                    ) : (
+                      <AiOutlineHeart size={24} />
+                    )}
+                  </div>
+                  <h5 className="card-title">{restaurant.restaurantName}</h5>
+                  <p className="m-0">
+                    Description:{" "}
+                    {restaurant.description || "No description available"}
+                  </p>
+                  <p className="m-0">
+                    Discount: {findDiscountForRestaurant(restaurant._id) || "0"}
+                    %
+                  </p>
+                  <p className="m-0">
+                    Location: {restaurant.restaurantAddress.split(",")[1]}
+                  </p>
+                  <p className="m-0">Cuisine: {restaurant.cuisine}</p>
+                  <p className="m-0">
+                    Ratings: {ratings[restaurant._id] || "Not Rated"}
+                  </p>
+                  <button className="btn btn-primary">Book</button>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </section>
+    </div>
+  );
 };
 
 export default Results;
