b821f863225c17b374d4b01f1bb0a29baf18b9e9
Venkata Sreenivas Prasad Kasibhatla <vn769140@dal.ca>
2024-04-03 16:23:31 -0300

    fix: Newsletter UI + Code fix

--- godine/src/views/dashboard/dashboard.jsx
+++ godine/src/views/dashboard/dashboard.jsx
@@ -1,163 +1,186 @@
-import React, { Component } from 'react';
-import CanvasJSReact from '@canvasjs/react-charts';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from "react";
+import CanvasJSReact from "@canvasjs/react-charts";
+import "bootstrap/dist/css/bootstrap.min.css";
+import axios from "axios";
+import useAuth from "../../hooks/useAuth";
 
-var CanvasJSChart = CanvasJSReact.CanvasJSChart;
-class Dashboard extends Component {
-    render() {
-        const bar= {
-            backgroundColor: null,
-            title: {
-                text: "Income chart"
-            },
-            data: [
-                {
-                    // Change type to "doughnut", "line", "splineArea", etc.
-                    type: "column",
-                    dataPoints: [
-                        { label: "January", y: 10 },
-                        { label: "February", y: 15 },
-                        { label: "March", y: 25 },
-                        { label: "April", y: 30 },
-                        { label: "May", y: 28 }
-                    ]
-                }
-            ]
-        }
-        const pie = {
+const CanvasJSChart = CanvasJSReact.CanvasJSChart;
 
-            backgroundColor: null,
-			exportEnabled: true,
-			animationEnabled: true,
-			title: {
-				text: "Booking Type"
-			},
-			data: [{
-				type: "pie",
-				startAngle: 75,
-				toolTipContent: "<b>{label}</b>: {y}%",
-				showInLegend: "true",
-				legendText: "{label}",
-				indexLabelFontSize: 16,
-				indexLabel: "{label} - {y}%",
-				dataPoints: [
-					{ y: 45, label: "Free Bookings" },
-					{ y: 55, label: "Paid Bookings" }
-				]
-			}]
-		}
-        const line = {
-			backgroundColor: null,
-			animationEnabled: true,
-			exportEnabled: true,
-			title: {
-				text: "Number of Bookings"
-			},
-			axisY: {
-				title: "Number of Bookings",
-			},
-			data: [
-			{
-				type: "area",
-				xValueFormatString: "YYYY",
-				yValueFormatString: "#,##0.## Million",
-				dataPoints: [
-					{ x: new Date(2017, 0), y: 7.6},
-					{ x: new Date(2016, 0), y: 7.3},
-					{ x: new Date(2015, 0), y: 6.4},
-					{ x: new Date(2014, 0), y: 5.3},
-					{ x: new Date(2013, 0), y: 4.5},
-					{ x: new Date(2012, 0), y: 3.8},
-					{ x: new Date(2011, 0), y: 3.2}
-				]
-			}
-			]
-		}
+const Dashboard = () => {
+  const { getUserId } = useAuth();
+  const userId = getUserId();
+  const barOptions = {
+    backgroundColor: null,
+    title: {
+      text: "Income Chart",
+    },
+    data: [
+      {
+        type: "column",
+        dataPoints: [
+          { label: "January", y: 10 },
+          { label: "February", y: 15 },
+          { label: "March", y: 25 },
+          { label: "April", y: 30 },
+          { label: "May", y: 28 },
+        ],
+      },
+    ],
+  };
 
-        return (
-            <div className='cb'>
-                <div className='row'>
-                    <div className='col-md-6 '>
-                        <div className='row my-auto justify-content-evenly my-auto mx-3'>
-                        <div className='card col-md-3  '>
-                            <div className='card-body text-center '>
-                                <h5 className=' card-title'>Average Order Value</h5>
-                                <p className=' card-text'>$ 50</p>
-                            </div>
-                        </div>
-                        <div className='card col-md-3 mx-5'>
-                            <div className='card-body text-center'>
-                                <h5 className='card-title'>Total Revenu</h5>
-                                <p className='card-text'>$ 3000</p>
-                            </div>
-                        </div>
-                        <div className='card col-md-3 mx-5'>
-                            <div className='card-body text-center'>
-                                <h5 className='card-title'>Average Ratings</h5>
-                                <p className='card-text '>3.9⭐</p>
-                            </div>
-                        </div>
-                        </div>
+  const pieOptions = {
+    backgroundColor: null,
+    exportEnabled: true,
+    animationEnabled: true,
+    title: {
+      text: "Booking Type",
+    },
+    data: [
+      {
+        type: "pie",
+        startAngle: 75,
+        toolTipContent: "<b>{label}</b>: {y}%",
+        showInLegend: "true",
+        legendText: "{label}",
+        indexLabelFontSize: 16,
+        indexLabel: "{label} - {y}%",
+        dataPoints: [
+          { y: 45, label: "Free Bookings" },
+          { y: 55, label: "Paid Bookings" },
+        ],
+      },
+    ],
+  };
 
-                        <div className='my-2 m-5 p-5'> <CanvasJSChart options={bar}/></div>
-                        <div className='my-2 m-5 p-5'> <CanvasJSChart options={pie}/></div>
-                        <div className='my-2 m-5 p-5'> <CanvasJSChart options={line}/></div>
-                    </div>
-                    <div className='col-md-6 p-5'>
-                        <form className='form'>
-                            <h1 className='text-center'>Add A Restaurant</h1>
-                            <div className='form-group'>
-                                <label htmlFor='restaurantName'>Restaurant Name</label>
-                                <input type='text' className='form-control' id='restaurantName' />
-                            </div>
-                            <div className='form-group'>
-                                <label htmlFor='address'>Address</label>
-                                <input type='text' className='form-control' id='address' />
-                            </div>
-                            <div className='form-group'>
-                                <label htmlFor='cuisines'>Cuisines</label>
-                                <input type='text' className='form-control' id='cuisines' />
-                            </div>
-                            <div className='form-group'>
-                                <label htmlFor='workingHours'>Working Hours</label>
-                                <input type='text' className='form-control' id='workingHours' />
-                            </div>
-                            <div className='form-group'>
-                                <label htmlFor='contactNumber'>Contact Number</label>
-                                <div className='input-group'>
-                                <div className='input-group-append'>
-                                        <select className='custom-select'>
-                                            <option value='1'>+1</option>
-                                            <option value='2'>+91</option>
-                                            <option value='3'>+111</option>
-                                            <option value='4'>+71</option>
-                                            <option value='5'>+60</option>
-                                        </select>
-                                    </div>
-                                    <input type='text' className='form-control' id='contactNumber' />
-  
-                                </div>
-                            </div>
-                            <div className='form-group'>
-                                <label htmlFor='seatingCapacity'>Seating Capacity</label>
-                                <input type='text' className='form-control' id='seatingCapacity' />
-                            </div>
-                            <div className='form-group'>
-                                <label htmlFor='menu'>Upload Menu</label>
-                                <input type='file' className='form-control-file' id='menu' />
-                            </div>
-                            <div className='form-group'>
-                                <label htmlFor='photos'>Upload Photos</label>
-                                <input type='file' className='form-control-file' id='photos' />
-                            </div>
-                            <div className='form-group text-center'>
-                                <button type='submit' className='btn btn-primary'>Add Restaurant</button>
-                            </div>
-                        </form>
-                    </div>
-                </div>
+  const lineOptions = {
+    backgroundColor: null,
+    animationEnabled: true,
+    exportEnabled: true,
+    title: {
+      text: "Number of Bookings",
+    },
+    axisY: {
+      title: "Number of Bookings",
+    },
+    data: [
+      {
+        type: "area",
+        xValueFormatString: "YYYY",
+        yValueFormatString: "#,##0.## Million",
+        dataPoints: [
+          { x: new Date(2017, 0), y: 7.6 },
+          { x: new Date(2016, 0), y: 7.3 },
+          { x: new Date(2015, 0), y: 6.4 },
+          { x: new Date(2014, 0), y: 5.3 },
+          { x: new Date(2013, 0), y: 4.5 },
+          { x: new Date(2012, 0), y: 3.8 },
+          { x: new Date(2011, 0), y: 3.2 },
+        ],
+      },
+    ],
+  };
+
+  return (
+    <div className="cb">
+      <div className="row">
+        <div className="col-md-6">
+          <div className="row my-auto justify-content-evenly my-auto mx-3">
+            <div className="card col-md-3">
+              <div className="card-body text-center">
+                <h5 className="card-title">Average Order Value</h5>
+                <p className="card-text">$50</p>
+              </div>
             </div>
-        )}
-}
+            <div className="card col-md-3 mx-5">
+              <div className="card-body text-center">
+                <h5 className="card-title">Total Revenue</h5>
+                <p className="card-text">$3000</p>
+              </div>
+            </div>
+            <div className="card col-md-3 mx-5">
+              <div className="card-body text-center">
+                <h5 className="card-title">Average Ratings</h5>
+                <p className="card-text">3.9⭐</p>
+              </div>
+            </div>
+          </div>
+          <div className="my-2 m-5 p-5">
+            {" "}
+            <CanvasJSChart options={barOptions} />
+          </div>
+          <div className="my-2 m-5 p-5">
+            {" "}
+            <CanvasJSChart options={pieOptions} />
+          </div>
+          <div className="my-2 m-5 p-5">
+            {" "}
+            <CanvasJSChart options={lineOptions} />
+          </div>
+        </div>
+        <div className="col-md-6 p-5">
+          <form className="form">
+            <h1 className="text-center">Add A Restaurant</h1>
+            <div className="form-group">
+              <label htmlFor="restaurantName">Restaurant Name</label>
+              <input type="text" className="form-control" id="restaurantName" />
+            </div>
+            <div className="form-group">
+              <label htmlFor="address">Address</label>
+              <input type="text" className="form-control" id="address" />
+            </div>
+            <div className="form-group">
+              <label htmlFor="cuisines">Cuisines</label>
+              <input type="text" className="form-control" id="cuisines" />
+            </div>
+            <div className="form-group">
+              <label htmlFor="workingHours">Working Hours</label>
+              <input type="text" className="form-control" id="workingHours" />
+            </div>
+            <div className="form-group">
+              <label htmlFor="contactNumber">Contact Number</label>
+              <div className="input-group">
+                <div className="input-group-prepend">
+                  <select className="custom-select">
+                    <option value="+1">+1</option>
+                    <option value="+91">+91</option>
+                    <option value="+111">+111</option>
+                    <option value="+71">+71</option>
+                    <option value="+60">+60</option>
+                  </select>
+                </div>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="contactNumber"
+                />
+              </div>
+            </div>
+            <div className="form-group">
+              <label htmlFor="seatingCapacity">Seating Capacity</label>
+              <input
+                type="text"
+                className="form-control"
+                id="seatingCapacity"
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="menu">Upload Menu</label>
+              <input type="file" className="form-control-file" id="menu" />
+            </div>
+            <div className="form-group">
+              <label htmlFor="photos">Upload Photos</label>
+              <input type="file" className="form-control-file" id="photos" />
+            </div>
+            <div className="form-group text-center">
+              <button type="submit" className="btn btn-primary">
+                Add Restaurant
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Dashboard;
--- godine/src/views/newsletter/newsletter.jsx
+++ godine/src/views/newsletter/newsletter.jsx
@@ -3,62 +3,20 @@ import { Card, Button, Container, Row, Col, Modal } from "react-bootstrap";
 import axios from "axios";
 import useAuth from "../../hooks/useAuth";
 import "./newsletter.css";
-import newsletter1 from "./../../images/newsletter1.png";
-import newsletter2 from "./../../images/newsletter2.png";
-import newsletter3 from "./../../images/newsletter3.png";
-import newsletter4 from "./../../images/newsletter4.png";
-import newsletter5 from "./../../images/newsletter5.png";
-import newsletter6 from "./../../images/newsletter6.png";
+import newsletter1 from "./../../images/newsletter1.webp";
+import newsletter2 from "./../../images/newsletter2.webp";
+import newsletter3 from "./../../images/newsletter3.webp";
+import newsletter4 from "./../../images/newsletter4.webp";
+import newsletter5 from "./../../images/newsletter5.webp";
+import newsletter6 from "./../../images/newsletter6.webp";
 
 const newsletterContents = {
-  1: `My previous quest for the ultimate bread in Greater Vancouver was so much fun that I thought I would do it again for another one of my favourite treats. Almond croissants have long been my go-to at cafes and bakeries. Sure, they are not as pure as a classic croissant but the almond variant is my preference. This hyper-local list is no casual walkthrough of the prettiest photos on Instagram. I actually ate and ranked every almond croissant that appears. Nothing was prepared especially for me. Almond Croissant Guide Vancouver
-  The Criteria:
-I am on a constant search for the best almond croissant amongst the plethora of bakeries in Greater Vancouver. This is a ranked list of my own tasting preferences. There will be a winner, two runner-ups, a cutoff for the ones I'd recommend, and many losers. To start off, here is my criteria for a good almond croissant:
-Must have a good rise 
-Must have enough almond on top and within. I'm not here for a sprinkling of nuts
-Must have a good balance of sweetness, nuttiness, and buttery croissant-ness
-Must break into satisfying flakes on impact with my teeth`,
-  2: `When an fancy concept restaurant opens in Vancouver, it's needs to do something special in order to stand out and distinguish itself from the plethora of fine dining options. Thankfully, that's exactly what Collective Hospitality has executed with the opening of their third restaurant. In this review, I am biased. I have already spent time in The Mackenzie Room and Say Mercy! Both had their unique twists, and "French-ish" is the parallel twist at Collective Good Bistro. You can deliberate between a tasting menu or order a la carte, which I love about dining in this family of restaurants, and tonight's meal was another satisfying victory for the local Vancouver scene.
-
-Collective Goods in Vancouver – Gnocchi Parisienne
-Food:
-Team Tastic came for an early Sunday dinner and I had a great taste of the following:
-
-Salmon "Meunière" ($20)
-Gnocchi Parisienne ($41)
-Duck ($46)
-The salmon "meunière" was a succulent and tender starter. The king salmon had a ton of fattiness, and when mixed in with fried caper berries, lemon caper vinaigrette, and candied black pepper lemon, was a playful appetizer that left me with a tart finish and a feeling of great anticipation for the rest of the meal.`,
-
-  3: `I am the type of person to consider giving a Japanese snack box as a gift so it's a little strange to be on the other end of receiving not one but TWO boxes from Sakuraco and TokyoTreats to try. While both are filled with Japanese snacks, the branding and snack contents are made for slightly different audiences. Both boxes are the same price points for all subscription levels and guarantee the delivery of authentic Japanese for your friends and loved ones. Join me in my discovery of both boxes this holiday season to see what's inside.
-
-Sakuraco Subscription Box
-Sakuraco Subscription Box
-Sakuraco
-Sakuraco (starting at $32.50 USD/mo for 12 months) specializes in a refined snack experience. This month's theme was "Holidays in Hokkaido". We're talking rice crackers, yuzu infusions, and straight up Japanese chinaware.`,
-
-  4: `Vancouver has its fair share of taco shops but these are a fast-fry experience whose number one mission is to deliver tortillas and protein into your stomach as fast as possible. Alimentaria Mexicana on Granville Island is a cantina that slows things down and gives diners a chance to try higher quality proteins and entrees. It'll cost more than your average taco shop, but there's nothing else like it in the area. Join me in my visit to Alimentaria Mexicana this past weekend to see if this is the experience for you.
-
-Alimentaria Mexicana – Lamb Belly Barbacoa
-Food:
-Team Tastic came here for a weekend lunch and I had a great taste of the following:
-
-Rajas Con Queso ($20)
-Veg Birria ($21)
-Lamb Belly Barbacoa ($33)
-The rajas con queso was our appetizer of choice and to be honest, I was hoping for something thicker. Sure, the poblano peppers, onions, crema, and cheese did have a cheesy flavour but it spilled right out of the corn tortillas because the queso was so wet. Maybe opt for the esquites instead if you're looking to share an appetizer.`,
-
-  5: `There are trucks that show up to take your lunch money in exchange for standard items and then there are trucks that are doing something really unique that leave you hoping they can set up a permanent brick-and-mortar location.
-
- Random Food Truck off the Coquihalla Highway
-Random Food Truck off the Coquihalla Highway
-This guide looks at the latter with the following strict criteria:
-Serving something not immediately available at a restaurant in the same neighbourhood
-Is not affiliated with a restaurant with a brick-and-mortar location (sorry Le Tigre)
-Is fairly available throughout the year and is not a limited time thing (sorry BBQ trucks at The PNE)
-Takeout from ghost kitchens aren't food trucks
- With this criteria in mind, there are both winners and losers because I spent my money and have opinions to share. Note that all prices noted are from the time from my visit and are subject to change`,
-
-  6: `In the vibrant culinary landscape of Vancouver, where Vietnamese eateries often don the monikers of "Pho _____" or embrace names reminiscent of Vietnamese phrases, emerges Wooden Fish — a serene enclave in the heart of West Vancouver's Ambleside neighborhood. Departing from the conventional nomenclature, this establishment derives its name from a profound Buddhist symbol cherished across various Asian cultures. If you're looking for beauty in Vietnamese food, located in a nice neighbourhood, and a hair below the prices of Anh + Chi who are now riding the wave of their Michelin Bib Gourmand designation, this might be a great spot to stop by.`,
+  1: `As the leaves change color and the air turns crisp, our menu undergoes its own transformation. This season, we're thrilled to introduce an array of dishes inspired by autumn's rich palette and flavors. Dive into the comforting embrace of our pumpkin spice risotto, or savor the delicate balance of our roasted duck with a cranberry glaze. Each dish is crafted with locally sourced ingredients, bringing the harvest to your table. Join us for a culinary journey that celebrates the very essence of fall. Don't wait to reserve your table — these seasonal specialties are here for a limited time only.`,
+  2: `This month, we're giving you a peek behind the curtain with an exclusive recipe from our esteemed Chef Maria. Known for her innovative use of traditional ingredients, Chef Maria shares her secret to the perfect beef bourguignon, a comforting dish that's perfect for the cooler weather. With a detailed guide and expert tips, you'll be able to recreate a taste of our kitchen in your own home. Impress your guests or treat your family to this sumptuous meal. Don't forget, the secret ingredient is always a pinch of love!`,
+  3: `Prepare for an enchanting evening as we transform our garden into a dining oasis under the stars. Our upcoming outdoor dining event promises a night of exquisite food, fine wine, and live music, all set against the backdrop of our beautifully lit garden. The evening's menu features a special selection of dishes that perfectly pair with our handpicked wines, promising a gastronomic adventure. Spaces are limited, so be sure to secure your spot for a magical night that celebrates the joy of dining al fresco.`,
+  5: `We're excited to announce the launch of our new vegan menu, crafted with creativity and care. Recognizing the growing demand for plant-based options, our chefs have poured their passion into developing a range of dishes that delight the senses without compromise. From our creamy cashew cheese lasagna to our decadent chocolate avocado mousse, each dish is a testament to the versatility and richness of vegan cuisine. Whether you're a long-time vegan or simply curious to explore, our new menu promises a culinary exploration that's as kind to the planet as it is to your palate.`,
+  4: `At the heart of our culinary philosophy is a commitment to supporting local farmers and producers. This month, we're excited to spotlight the heroes who supply us with the freshest and finest ingredients. Join us for a special farm-to-table experience, where each dish tells the story of local agriculture and sustainable farming practices. Our specially curated menu highlights the flavors of the region, showcasing the dedication and hard work of our farming partners. Book your table now and be part of a dining experience that honors the local land and its bounty.`,
+  6: `Join us for an exclusive evening dedicated to the art of wine. Our upcoming wine tasting event features a selection of exquisite wines from around the globe, carefully paired with artisanal cheeses and gourmet bites. Led by our sommelier, this intimate gathering is an opportunity to deepen your appreciation of wine, learn about different varietals, and discover the stories behind the labels. Spaces for this special event are limited, ensuring a personalized experience for each of our guests. Reserve your spot now and raise a glass to an unforgettable journey through the world of wine.`,
 };
 
 const newsletterImages = {
@@ -68,6 +26,15 @@ const newsletterImages = {
   4: newsletter4,
   5: newsletter5,
   6: newsletter6,
+};
+
+const names = {
+  1: "Harvest Delights",
+  2: "Behind the Apron",
+  3: "Enchanted Evenings",
+  4: "From Farm to Feast",
+  5: "Green Gourmet",
+  6: "Vines & Vintages",
 };
 
 const Newsletter = () => {
@@ -142,14 +109,18 @@ const Newsletter = () => {
 
   return (
     <Container className="newsletter-page">
-      <h1 className="newsletter-header text-center my-4">Newsletters</h1>
+      <h1 className="newsletter-header text-center my-4">
+        <b>Top Newsletters of the Month</b>
+      </h1>
       <Row>
         {Object.entries(newsletterImages).map(([id, src]) => (
           <Col md={4} key={id}>
             <Card className="mb-4" onClick={() => handleCardClick(id)}>
               <Card.Img variant="top" src={src} />
               <Card.Body>
-                <Card.Title>{`Newsletter ${id}`}</Card.Title>
+                <Card.Title>
+                  <b>{`${names[id]}`}</b>
+                </Card.Title>
               </Card.Body>
             </Card>
           </Col>
@@ -169,7 +140,7 @@ const Newsletter = () => {
       {selectedNewsletterId && (
         <Modal show={showModal} onHide={() => setShowModal(false)} centered>
           <Modal.Header closeButton>
-            <Modal.Title>{`Newsletter ${selectedNewsletterId}`}</Modal.Title>
+            <Modal.Title>{`Newsletter : ${names[selectedNewsletterId]}`}</Modal.Title>
           </Modal.Header>
           <Modal.Body>{newsletterContents[selectedNewsletterId]}</Modal.Body>
         </Modal>
