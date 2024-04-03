4b9c864de3226ad4b466855e560957b3d06055e4
Priyatam Reddy Somagattu <tech.nerd141@gmail.com>
2024-04-02 21:11:03 -0300

    feat: added phone number field to sign up

--- godine/src/views/authentication/signup.jsx
+++ godine/src/views/authentication/signup.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useRef, useEffect } from 'react'
-import {useNavigate} from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 import useAuth from '../../hooks/useAuth';
 import './common.css'
 import axios from 'axios';
@@ -20,6 +20,9 @@ function Signup() {
     const [validPassword, setValidPassword] = useState(true);
     const [passwordMatch, setPasswordMatch] = useState(true);
 
+    const [phoneNumber, setPhoneNumber] = useState('');
+    const [validPhoneNumber, setValidPhoneNumber] = useState(true);
+
     const [toggleValue, setToggleValue] = useState('user');
 
     const { getAuthData, setAuthData, isSessionValid } = useAuth();
@@ -27,53 +30,53 @@ function Signup() {
     const navigate = useNavigate();
 
     useEffect(() => {
-        if(isSessionValid()){
-            navigate(getAuthData()?.role === "user"? "/": "/dashboard", {replace: true});
+        if (isSessionValid()) {
+            navigate(getAuthData()?.role === "user" ? "/" : "/dashboard", { replace: true });
         }
 
         firstNameRef.current.focus();
     }, []);
 
-    function validateFirstNameAndSet(e){
+    function validateFirstNameAndSet(e) {
         const inputFn = e.target.value;
         const fName = inputFn.trim();
         const fNameRegex = /^[a-zA-Z ]+$/;
 
-        if(fNameRegex.test(fName)){
+        if (fNameRegex.test(fName)) {
             setValidFirstName(true);
-            setFirstName(fName);
         } else {
             setValidFirstName(false);
         }
+        setFirstName(fName);
     }
 
-    function validateLastNameAndSet(e){
+    function validateLastNameAndSet(e) {
         const inputLn = e.target.value;
         const lName = inputLn.trim();
         const lNameRegex = /^[a-zA-Z ]+$/;
 
-        if(lNameRegex.test(lName)){
+        if (lNameRegex.test(lName)) {
             setValidLastName(true);
-            setLastName(lName);
         } else {
             setValidLastName(false);
         }
+        setLastName(lName);
     }
 
     function validateEmailAndSet(e) {
         const inputEmail = e.target.value;
-        
+
         const email = inputEmail.trim();
         // email is an email
         const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
 
         if (emailRegex.test(email)) {
             setValidEmail(true);
-            setEmail(email);
         }
         else {
             setValidEmail(false);
         }
+        setEmail(email);
     }
 
     function validatePasswordAndSet(e) {
@@ -82,24 +85,37 @@ function Signup() {
 
         if (pwdRegex.test(pwd)) {
             setValidPassword(true);
-            setPassword(pwd);
         }
         else {
             setValidPassword(false);
         }
+        setPassword(pwd);
     }
 
-    function validateConfirmPassword(e){
+    function validateConfirmPassword(e) {
         const confirmedPwd = e.target.value;
-        if(confirmedPwd.match(password)){
+        if (confirmedPwd.match(password)) {
             setPasswordMatch(true);
         }
-        else{
+        else {
             setPasswordMatch(false);
         }
     }
 
-    async function handleSubmit(e){
+    function validatePhoneNumberAndSet(e) {
+        const phoneNumber = (e.target.value).trim();
+        const phoneNumberRegex = /^(\+[1]\s?)?\d{3}[\s.-]?\d{3}[\s.-]?\d{4}$/;
+        
+        if (phoneNumberRegex.test(phoneNumber)) {
+            setValidPhoneNumber(true);
+        }
+        else {
+            setValidPhoneNumber(false);
+        }
+        setPhoneNumber(phoneNumber);
+    }
+
+    async function handleSubmit(e) {
         e.preventDefault();
 
         reqCancelRef.current?.abort();
@@ -109,59 +125,57 @@ function Signup() {
         const server_port = process.env.REACT_APP_SERVER_PORT || "8080";
         const signup_endpoint = process.env.REACT_APP_SIGNUP_ENDPOINT || "api/auth/signup";
 
-        if(firstName !== '' && lastName !== '' && email !== '' && password !== '' 
-            && validFirstName && validLastName && validEmail && validPassword && passwordMatch){
+        if (firstName !== '' && lastName !== '' && email !== '' && password !== '' && phoneNumber !== ''
+            && validFirstName && validLastName && validEmail && validPassword && passwordMatch && validPhoneNumber) {
 
             const url = `${server_url}:${server_port}/${signup_endpoint}`;
             const data = {
-                firstName: firstName, 
-                lastName: lastName, 
-                email: email, 
-                password: password, 
+                firstName: firstName,
+                lastName: lastName,
+                email: email,
+                password: password,
+                phoneNumber: phoneNumber,
                 role: toggleValue
             };
 
-            const response = await axios.post(url, data, {signal: reqCancelRef.current?.signal})
-                    .then((response) => response) 
-                    .catch((err) => {
-                        if(axios.isCancel(err)){
-                            return err;
-                        }
-                        if(axios.isAxiosError(err)){
-                            return err.response;
-                        }
-                        return err;
-                    });
+            const response = await axios.post(url, data, { signal: reqCancelRef.current?.signal })
+                .then((response) => response)
+                .catch((err) => err);
 
-            if(axios.isCancel(response)){
+            if (axios.isCancel(response)) {
                 console.log("Sign up request aborted");
                 return;
             }
 
-            if(response.status === 201 && response.data?.token && response.data?.data?.user?.role){
-                console.log('User created successfully');
-                const token =  response.data.token;
-                const role = response.data.data.user.role;
+            if (axios.isAxiosError(response)){
+                const err = response;
+                if(err.response.status === 401){
+                    alert("User already exists");
+                }
+                if(err.response.status === 500){
+                    alert("Error creating user, please try again later.");
+                }
+                window.location.reload();
+                return;
+            }
 
-                setAuthData(token, role);
+            if (response.status === 201 && response.data?.user) {
+                alert(`${response.data.message}, redirecting to sign in page`);
                 // forcing user to sign in to reinforce login credentials
-                navigate('/signin', {replace: true});
+                navigate('/signin', { replace: true });
             }
-            else if(response.status === 401){
-                alert("User already exists");
-            }
-            else{
+            else {
                 alert("Error creating user, please try again later.")
             }
             window.location.reload();
         }
-        else{
+        else {
             alert("Invalid field(s), please enter valid values.");
             window.location.reload();
         }
     }
 
-    function handleToggle(){
+    function handleToggle() {
         setToggleValue(prevValue => prevValue === 'user' ? 'restaurant owner' : 'user');
     }
 
@@ -172,45 +186,50 @@ function Signup() {
                 <div className='row justify-content-center'>
                     <div className='col-md-6'>
                         <div className='card signup-card glass'>
-                        <div className="tab-content">
-                            <h2 style={{color: "#333333"}}>Sign Up</h2>
-                            <form>
-                                <div className='form-group m-1'>
-                                    <label>First Name</label>
-                                    <input type='text' ref={firstNameRef} className='form-control' placeholder='Enter first name' onInput={validateFirstNameAndSet}/>
-                                    {validFirstName ? null : <small style={{color: 'red'}}>Please enter a valid first name</small>}
-                                </div>
-                                <div className='form-group m-1'>
-                                    <label>Last Name</label>
-                                    <input type='text' className='form-control' placeholder='Enter last name' onInput={validateLastNameAndSet}/>
-                                    {validLastName ? null : <small style={{color: 'red'}}>Please enter a valid last name</small>}
-                                </div>
-                                <div className='form-group m-1'>
-                                    <label>Email</label>
-                                    <input type='text' className='form-control' placeholder='Enter email' onInput={validateEmailAndSet}/>
-                                    {validEmail ? null : <small style={{color: 'red'}}>Please enter a valid email</small>}
-                                </div>
-                                <div className='form-group m-1'>
-                                    <label>Password</label>
-                                    <input type='password' className='form-control' placeholder='Enter password' onInput={validatePasswordAndSet}/>
-                                    {validPassword ? null : <small style={{color: 'red'}}>Passwords must be minimum 8 characters</small>}
-                                </div>
-                                <div className='form-group m-1'>
-                                    <label>Confirm password</label>
-                                    <input type='password' className='form-control' placeholder='Confirm password' onInput={validateConfirmPassword}/>
-                                    {passwordMatch ? null : <small style={{color: 'red'}}>Passwords do not match</small>}
-                                </div>
+                            <div className="tab-content">
+                                <h2 style={{ color: "#333333" }}>Sign Up</h2>
+                                <form>
+                                    <div className='form-group m-1'>
+                                        <label>First Name</label>
+                                        <input type='text' ref={firstNameRef} className='form-control' placeholder='Enter first name' onInput={validateFirstNameAndSet} value={firstName} />
+                                        {validFirstName ? null : <small style={{ color: 'red' }}>Please enter a valid first name</small>}
+                                    </div>
+                                    <div className='form-group m-1'>
+                                        <label>Last Name</label>
+                                        <input type='text' className='form-control' placeholder='Enter last name' onInput={validateLastNameAndSet} value={lastName} />
+                                        {validLastName ? null : <small style={{ color: 'red' }}>Please enter a valid last name</small>}
+                                    </div>
+                                    <div className='form-group m-1'>
+                                        <label>Email</label>
+                                        <input type='text' className='form-control' placeholder='Enter email' onInput={validateEmailAndSet} value={email}/>
+                                        {validEmail ? null : <small style={{ color: 'red' }}>Please enter a valid email</small>}
+                                    </div>
+                                    <div className='form-group m-1'>
+                                        <label>Phone number</label>
+                                        <input type='text' className='form-control' placeholder='Enter phone number' onInput={validatePhoneNumberAndSet} value={phoneNumber}/>
+                                        {validPhoneNumber ? null : <small style={{ color: 'red' }}>Please enter a valid Canadian phone number</small>}
+                                    </div>
+                                    <div className='form-group m-1'>
+                                        <label>Password</label>
+                                        <input type='password' className='form-control' placeholder='Enter password' onInput={validatePasswordAndSet} value={password} />
+                                        {validPassword ? null : <small style={{ color: 'red' }}>Passwords must be minimum 8 characters</small>}
+                                    </div>
+                                    <div className='form-group m-1'>
+                                        <label>Confirm password</label>
+                                        <input type='password' className='form-control' placeholder='Confirm password' onInput={validateConfirmPassword} />
+                                        {passwordMatch ? null : <small style={{ color: 'red' }}>Passwords do not match</small>}
+                                    </div>
 
-                                <div className='toggle-container'>
-                                    <span className='toggle-value'>User</span>
-                                    <label className='switch'>
-                                        <input type='checkbox' onChange={handleToggle} />
-                                        <span className='slider round' />
-                                    </label>
-                                    <span className='toggle-value'>Restaurant owner</span>
-                                </div>
-                                <button type='submit' className='btn btn-primary m-1' onClick={handleSubmit} >Submit</button>
-                            </form>
+                                    <div className='toggle-container'>
+                                        <span className='toggle-value'>User</span>
+                                        <label className='switch'>
+                                            <input type='checkbox' onChange={handleToggle} />
+                                            <span className='slider round' />
+                                        </label>
+                                        <span className='toggle-value'>Restaurant owner</span>
+                                    </div>
+                                    <button type='submit' className='btn btn-primary m-1' onClick={handleSubmit} >Submit</button>
+                                </form>
                             </div>
                         </div>
                     </div>
