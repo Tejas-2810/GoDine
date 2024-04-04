193ee6f83f101ac07f44eba0dbb16081af45c816
VenkataSreenivasPrasad <137905440+SreenivasPrasad2025@users.noreply.github.com>
2024-04-03 23:10:34 -0300

    Update newsletterController.js

--- godine_server/src/controllers/newsletterController.js
+++ godine_server/src/controllers/newsletterController.js
@@ -1,4 +1,3 @@
-// newsletterController.js
 const nodemailer = require("nodemailer");
 const cron = require("node-cron");
 const Users = require("../models/users");
@@ -44,15 +43,9 @@ const getAllData = async (req, res) => {
 const unSubscribeToNewsletter = async (req, res) => {
   try {
     const { userID } = req.body;
-    // const user = await Users.findOne({ email: email });
-    // if (!user) {
-    //   return res.status(404).json({ message: "User not found" });
-    // }
     console.log(userID);
 
     let newsletter = await Newsletter.findOneAndDelete({ userID: userID });
-
-    // Removed the newsletter.save() line because it's not needed when deleting documents
     res.status(200).json({ message: "Unsubscribed" });
   } catch (err) {
     res.status(500).json({ message: err.message });
@@ -62,10 +55,6 @@ const unSubscribeToNewsletter = async (req, res) => {
 const subscribeToNewsletter = async (req, res) => {
   try {
     const { userID } = req.body;
-    // const user = await Users.findOne({ email: email });
-    // if (!user) {
-    //   return res.status(404).json({ message: "User not found" });
-    // }
 
     let newsletter = await Newsletter.findOne({ userID: userID });
     if (!newsletter) {
@@ -86,7 +75,7 @@ const subscribeToNewsletter = async (req, res) => {
 
 const initNewsletterSchedule = () => {
   cron.schedule(
-    "*/2 * * * *",
+    "30 10 * * 1",
     async () => {
       try {
         const newsletters = await Newsletter.find({
@@ -95,7 +84,10 @@ const initNewsletterSchedule = () => {
         for (const newsletter of newsletters) {
           const user = await Users.findById(newsletter.userID);
           if (user && user.email) {
-            const content = "This is your newsletter content!";
+            const content =
+              "Hello " +
+              user.name +
+              ",\n\nThis week's newsletter: \n\n When an fancy concept restaurant opens in Vancouver, it's needs to do something special in order to stand out and distinguish itself from the plethora of fine dining options. Thankfully, that's exactly what Collective Hospitality has executed with the opening of their third restaurant. In this review, I am biased. I have already spent time in The Mackenzie Room and Say Mercy! Both had their unique twists, and French-ish is the parallel twist at Collective Good Bistro. You can deliberate between a tasting menu or order a la carte, which I love about dining in this family of restaurants, and tonight's meal was another satisfying victory for the local Vancouver scene. Collective Goods in Vancouver – Gnocchi Parisienne Food: Team Tastic came for an early Sunday dinner and I had a great taste of the following: Salmon Meunière ($20) Gnocchi Parisienne ($41) Duck ($46) The salmon meunière was a succulent and tender starter. The king salmon had a ton of fattiness, and when mixed in with fried caper berries, lemon caper vinaigrette, and candied black pepper lemon, was a playful appetizer that left me with a tart finish and a feeling of great anticipation for the rest of the meal.\n\n";
             await sendNewsletterEmail(user.email, content);
           }
         }
