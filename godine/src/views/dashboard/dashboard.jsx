84d3bbd0453a3063cb55342d559bfef40d335f08
Krishna Vaibhav Yadlapalli <kr732880@dal.ca>
2024-04-12 21:05:40 -0300

    modified code

--- godine/src/views/dashboard/dashboard.jsx
+++ godine/src/views/dashboard/dashboard.jsx
@@ -1,8 +1,10 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import CanvasJSReact from "@canvasjs/react-charts";
 import "bootstrap/dist/css/bootstrap.min.css";
-import axios from "axios";
+import axios, { isAxiosError } from "axios";
 import useAuth from "../../hooks/useAuth";
+import { useSearchParams, useNavigate, useLocation } from "react-router-dom";
+
 
 const CanvasJSChart = CanvasJSReact.CanvasJSChart;
 const server_url = process.env.REACT_APP_SERVER_URL || "http://localhost:8080";
@@ -10,21 +12,138 @@ const server_url = process.env.REACT_APP_SERVER_URL || "http://localhost:8080";
 const Dashboard = () => {
   const { getUserId } = useAuth();
   const userId = getUserId();
+  // const userId = "660e2a0646c439afcb241d1e";
+  const cancelRequestRef = useRef(null);
+  const [searchParams] = useSearchParams();
+  const [rdata, setRdata] = useState([]);
+  const [bdata, setBdata] = useState([]);
+  const [pdata, setPdata] = useState([]);
+  const navigate = useNavigate();
+  const [ar, setAr] = useState(0);
+  const [tb, setTb] = useState(0);
+
+
+  useEffect(() => {
+    const server_url = process.env.REACT_APP_SERVER_URL || "http://localhost:8080";
+    const ear = `${server_url}/api/restaurants/overall-averagerating/${userId}`;
+    const etb = `${server_url}/api/restaurants/total-bookings/${userId}`;    
+    axios.get(ear).then((response) => {
+      const ar = response.data.overallAverageRating;
+      setAr(ar);
+    });
+    axios.get(etb).then((response) => {
+      const tb = response.data.totalBookings;
+      setTb(tb);
+    });
+    
+    
+    cancelRequestRef.current?.abort();
+    cancelRequestRef.current = new AbortController();
+    const fetchData = async () => {
+      const resturant_endpoint = process.env.REACT_APP_PROFILE_ENDPOINT || "api/restaurants/restaurantratings";
+      const endpoint = `${server_url}/${resturant_endpoint}/${userId}`;
+      try {
+        const token = sessionStorage.getItem("token");
+        const headers = {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        };
+        const response = await axios
+          .get(endpoint, {
+            signal: cancelRequestRef.current?.signal,
+            headers: headers,
+          })
+          .then((response) => response)
+          .catch((err) => err);
+        setRdata(response.data);
+      } catch (error) {
+        console.error("Error fetching restaurant data:", error);
+      }
+    };
+
+    const fetchData2 = async () => {
+      const resturant_endpoint = process.env.REACT_APP_PROFILE_ENDPOINT || "api/restaurants/numberOfrestaurantbookings";
+      const endpoint = `${server_url}/${resturant_endpoint}/${userId}`;
+      try {
+        const token = sessionStorage.getItem("token");
+        const headers = {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        };
+        const response = await axios
+          .get(endpoint, {
+            signal: cancelRequestRef.current?.signal,
+            headers: headers,
+          })
+          .then((response) => response)
+          .catch((err) => err);
+        setBdata(response.data);
+      } catch (error) {
+        console.error("Error fetching restaurant data:", error);
+      }
+    };
+
+    const fetchData3 = async () => {
+      const resturant_endpoint = process.env.REACT_APP_PROFILE_ENDPOINT || "api/restaurants/bookingpercentages";
+      const endpoint = `${server_url}/${resturant_endpoint}/${userId}`;
+      try {
+        const token = sessionStorage.getItem("token");
+        const headers = {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        };
+        const response = await axios
+          .get(endpoint, {
+            signal: cancelRequestRef.current?.signal,
+            headers: headers,
+          })
+          .then((response) => response)
+          .catch((err) => err);
+        setPdata(response.data);
+      } catch (error) {
+        console.error("Error fetching restaurant data:", error);
+      }
+    };
+
+
+    fetchData();
+    fetchData2();
+    fetchData3();
+  }, []);
+  console.log("data----"+ar, tb)
+
+  var gone = [];
+  var gtwo = [];
+  var gthree = [];
+  if (rdata) {
+    gone = rdata.map((item) => {
+      return { label: item.restaurantName, y: item.averageRating };
+    });
+  }
+  if (bdata) {
+    gtwo = bdata.map((item) => {
+      return { label: item.restaurantName, y: item.numberOfBookings };
+    });
+  }
+  if (pdata) {
+    gthree = [
+      { label: "Paid Booking", y: parseFloat(pdata.paidBookingPercentage) },
+      { label: "Free Booking", y: parseFloat(pdata.freeBookingPercentage) },
+    ];
+  }
+
   const barOptions = {
     backgroundColor: null,
     title: {
-      text: "Income Chart",
+      text: "Average Ratings",
     },
     data: [
       {
         type: "column",
-        dataPoints: [
-          { label: "January", y: 10 },
-          { label: "February", y: 15 },
-          { label: "March", y: 25 },
-          { label: "April", y: 30 },
-          { label: "May", y: 28 },
-        ],
+        dataPoints: gone.map((item) => ({
+          label: item.label,
+          y: item.y,
+        })),
       },
     ],
   };
@@ -73,14 +192,29 @@ const Dashboard = () => {
         legendText: "{label}",
         indexLabelFontSize: 16,
         indexLabel: "{label} - {y}%",
-        dataPoints: [
-          { y: 45, label: "Free Bookings" },
-          { y: 55, label: "Paid Bookings" },
-        ],
+        dataPoints: gthree.map((item) => ({
+          label: item.label,
+          y: item.y,
+        })),
       },
     ],
   };
 
+  const barOptions2 = {
+    backgroundColor: null,
+    title: {
+      text: "Average Bookings",
+    },
+    data: [
+      {
+        type: "column",
+        dataPoints: gtwo.map((item) => ({
+          label: item.label,
+          y: item.y,
+        })),
+      },
+    ],
+  };
   const lineOptions = {
     backgroundColor: null,
     animationEnabled: true,
@@ -114,22 +248,16 @@ const Dashboard = () => {
       <div className="row">
         <div className="col-md-6">
           <div className="row my-auto justify-content-evenly my-auto mx-3">
-            <div className="card col-md-3">
+            <div className="card col-md-5">
               <div className="card-body text-center">
-                <h5 className="card-title">Average Order Value</h5>
-                <p className="card-text">$50</p>
+                <h5 className="card-title">Total Bookings</h5>
+                <p className="card-text">{tb}</p>
               </div>
             </div>
-            <div className="card col-md-3 mx-5">
-              <div className="card-body text-center">
-                <h5 className="card-title">Total Revenue</h5>
-                <p className="card-text">$3000</p>
-              </div>
-            </div>
-            <div className="card col-md-3 mx-5">
+            <div className="card col-md-5 ">
               <div className="card-body text-center">
                 <h5 className="card-title">Average Ratings</h5>
-                <p className="card-text">3.9⭐</p>
+                <p className="card-text">{ar}⭐</p>
               </div>
             </div>
           </div>
@@ -143,7 +271,7 @@ const Dashboard = () => {
           </div>
           <div className="my-2 m-5 p-5">
             {" "}
-            <CanvasJSChart options={lineOptions} />
+            <CanvasJSChart options={barOptions2} />
           </div>
         </div>
         <div className="col-md-6 p-5">
